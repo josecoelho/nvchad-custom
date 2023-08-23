49b02063dcb44d4a5d8aeac4ab0e6d8d54cb9eba
José Coelho <jose.coelho@palomagroup.com>
2023-08-23 20:32:38 +1200

    First commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# neovim [NvChad](https://nvchad.github.io/) custom config
+
+## Installation
+
+Install NVChad
+
+```sh
+git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1 && nvim
+```
+
+Then install this custom repo
+
+
+```sh
+rm -r ~/.config/nvim/lua/custom
+git clone https://github.com/reaink/nvchad-custom ~/.config/nvim/lua/custom
+nvim
+```
---
+++ plugins.lua
@@ -0,0 +1,58 @@
+local overrides = require("custom.configs.overrides")
+
+---@type NvPluginSpec[]
+local plugins = {
+
+  -- Tmux integration
+  {
+    "christoomey/vim-tmux-navigator",
+    event='VeryLazy',
+  },
+  {
+    'tmux-plugins/vim-tmux-focus-events',
+    event='VeryLazy',
+  },
+  {
+    "tpope/vim-surround",
+    event="VeryLazy",
+  },
+  -- Git wrapper
+  {
+    "tpope/vim-fugitive",
+    event="VeryLazy",
+  },
+  -- :GBrowse bit bucket
+  {
+    "tommcdo/vim-fubitive",
+    event="VeryLazy",
+  },
+  {
+    "jackMort/ChatGPT.nvim",
+    event = 'VeryLazy',
+    config = function()
+      require("chatgpt").setup()
+    end,
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim"
+    }
+  }
+
+
+  -- To make a plugin not be loaded
+  -- {
+  --   "NvChad/nvim-colorizer.lua",
+  --   enabled = false
+  -- },
+
+  -- All NvChad plugins are lazy-loaded by default
+  -- For a plugin to be loaded, you will need to set either `ft`, `cmd`, `keys`, `event`, or set `lazy = false`
+  -- If you want a plugin to load on startup, add `lazy = false` to a plugin spec, for example
+  -- {
+  --   "mg979/vim-visual-multi",
+  --   lazy = false,
+  -- }
+}
+
+return plugins
